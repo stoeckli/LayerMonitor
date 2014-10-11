2389b2bd0c2fb48cde60dba174f0d014d47ea5fe
stoeckli <email@stoeckli.net>
2014-10-11 12:21:26 +0200

    removed timer, display OK

--- LayerMonitor/LayerMonitor.c
+++ LayerMonitor/LayerMonitor.c
@@ -21,8 +21,8 @@
 #define Key_Port_Write			PORTB
 #define Key_Port_Read			PINB
 
-#define	k1						1		//key pins, Port B						
-#define k2						2
+#define	kmode					1		//key pins, Port B						
+#define kreset					2
 #define kmenu					3
 #define kok						4
 
@@ -31,32 +31,14 @@
 #define p_batt_en				1
 #define p_batt				    0
 
-/*
-#define th						0x84	//LCD char pos
-#define tm  					0x87					
-#define ts						0x8A
-#define t1h						0xC0
-#define t1m						0xC2
-#define t1s						0xC5
-#define t2h						0xC9
-#define t2m						0xCB
-#define t2s						0xCE
-*/
-
 #define th						0x86	//LCD char pos
 #define tm  					0x89
 #define ts						0x90
-#define t1h						0xD0
-#define t1m						0xC0
-#define t1s						0xC3
-#define t1al					0x80
-#define t2h						0xD0
-#define t2m						0xCB
-#define t2s						0xCE
-#define t2al					0x8F
+#define lcd_l1					0x80
+#define lcd_l2					0xC0
 
-#define sym_flame1				0x00	//symbols
-#define sym_flame2				0x01
+#define sym_micro				0x00	//symbols
+#define sym_squ					0x01
 #define sym_empty				0x02
 #define sym_half				0x03
 #define sym_full				0x04
@@ -64,9 +46,8 @@
 #define sym_speaker				0x06
 #define sym_break				0x07
 
-
-static unsigned char pattern0[8] = {2,4,12,30,31,31,14,0} ; 		//flame 1
-static unsigned char pattern1[8] = {0,4,6,14,31,31,14,0} ; 		// flame 2
+static unsigned char pattern0[8] = {17,17,17,25,22,16,16,0} ; 	//micro
+static unsigned char pattern1[8] = {4,10,2,4,14,0,0,0} ; 		// squ
 static unsigned char pattern2[8] = {12,30,18,18,18,18,30,0} ; 	//empty
 static unsigned char pattern3[8] = {12,30,18,18,30,30,30,0} ; 	//half
 static unsigned char pattern4[8] = {12,30,30,30,30,30,30,0} ; 	//full
@@ -84,6 +65,7 @@ unsigned char rchar;
 unsigned char LCD_PWR;
 unsigned char lcddata[0]; 
 
+
 typedef struct{ 
 unsigned char   second;      //enter the current time, date, month, and year 
 unsigned char   minute; 
@@ -91,11 +73,9 @@ unsigned char   hour;
 unsigned char   date; 
 unsigned char   month; 
 unsigned int    year;
-unsigned char   run
+unsigned char   run;
          }time; 
 time t;
-time t1;
-time t2;
 
 unsigned char menu;
 unsigned char timer_LCD;
@@ -104,44 +84,39 @@ unsigned char timer_menu;
 unsigned char timer_key;
 unsigned char speaker;
 unsigned char light;
-unsigned char alarm_mode_1;
-unsigned char alarm_mode_2;
-unsigned char alarm_time_1;
-unsigned char alarm_time_2;
 unsigned char battery;
 unsigned char contrast;
 unsigned char cc;
+unsigned char mode;
 
 int batt;
-
+long f_rel;
+long f_abs;
+long l_rel;
+long l_abs;
 
 int main(void)             
 { 
-	 
- 
   DDRB=0x01;
   PORTB=0x3E;
 
   DDRC=0xFE;
   PORTC=0x06;
-
    
   tchar = ':';
-  alarm_mode_1 = 0x01;
-  alarm_mode_2 = 0x02;
-  alarm_time_1 = 10;
-  alarm_time_2 = 10; 
+
   menu = 0;
   timer_menu = 0;
   contrast = 2;
   timer_LCD = 10;
   timer_key = 0;
+  mode = 'A';
+  f_rel = 0;
+  l_rel = 0;
   
   LCD_Wake();
 
   //LCD_Port_Write |= (1<<LCD_BL)
-
- 
 
 	PCMSK0 |= (1<<PCINT1); //  tell pin change mask to listen to pin15
    	PCMSK0 |= (1<<PCINT2); //  tell pin change mask to listen to pin16
@@ -170,11 +145,11 @@ int main(void)
     while(1) 
     { 
      
-	    OCR2A = 0;                       // Dummyzugriff
-        while((ASSR & (1<< OCR2AUB)));   // Warte auf das Ende des Zugriffs
+	    OCR2A = 0;                       // dummy access
+        while((ASSR & (1<< OCR2AUB)));   // wait for finish
  
         set_sleep_mode(SLEEP_MODE_PWR_SAVE);
-        sleep_mode();                   // in den Schlafmodus wechseln
+        sleep_mode();                   // chnage to sleep mode
  
     } 
 return 0; 
@@ -189,43 +164,37 @@ ISR(TIMER2_OVF_vect)         //overflow interrupt vector
 	{
 		timer_key = 1;
 
-		if ((PINB & (1<<k1)) == 0)         //timer1
+		if ((PINB & (1<<kmode)) == 0)         //no use 
 	    { 
-			t1.second=0;
-			t1.minute=0;
-			t1.hour=0;
-			Write_LCD (t1m,0);
-			LCD_print_str ("     ");
-			t1.run = (0x00);
+
+
 	    }
 
-		if ((PINB & (1<<k2)) == 0)         //timer2 
+		if ((PINB & (1<<kreset)) == 0)         //reset thickness
 	    { 
-			t2.second=0;
-			t2.minute=0;
-			t2.hour=0;
-			Write_LCD (t2m,0);
+			Write_LCD (0x80,0);
 			LCD_print_str ("     ");
-			t2.run = (0x00);
 	    }
 		
-		if (((PINB & (1<<kok)) == 0) & (menu == 0))         //timer1
+		if (((PINB & (1<<kok)) == 0) & (menu == 0))         //sleep
 	    { 
 			LCD_Sleep();
 	    }
 		
-		if (((PINB & (1<<kmenu)) == 0) & (menu == 0))         //timer1
+		if (((PINB & (1<<kmenu)) == 0) & (menu == 0))         //enter menu
 	    { 
 			 switch (menu)
 			 {
 				case 0:
-					++menu;
-					Write_LCD (0x80,0);
-	  				LCD_print_str   ("Alarm 1 Modus: ");
-					Write_LCD (0x8F,0);
-					LCD_out_alarm_sym (alarm_mode_1);
-					break;
-			 }
+				++menu;
+				Write_LCD (0x80,0);
+				LCD_print_str   ("Kontrast:      ");
+				Write_LCD (0x8F,0);
+				Write_LCD (contrast+48,1);
+				comp_contrast(contrast);
+
+				break;
+			}
 
 		}
 	}
@@ -236,7 +205,6 @@ ISR(TIMER2_OVF_vect)         //overflow interrupt vector
 		if (--timer_menu == 0)
 		{
 			menu = 0;
-			LCD_Time();
 		}
 
 	}
@@ -256,7 +224,6 @@ ISR(TIMER2_OVF_vect)         //overflow interrupt vector
             }
 			if (LCD_PWR != 0 & menu == 0)
 			{
-				LCD_out_h();
 				batt = battery_voltage();
 				battery_symbol();
 				comp_contrast(contrast);
@@ -284,102 +251,33 @@ ISR(TIMER2_OVF_vect)         //overflow interrupt vector
 
 		}
 
-		if (LCD_PWR != 0 & menu == 0) LCD_out_m();
     } 
 	
 	if (LCD_PWR == 0x00) return;
  
 	PORTC |= (1<<p_buzzer); 	
 
-	if (tchar == ':')
+	if (mode=='A')
 	{
-		tchar = ' ';
-		rchar = 0x00;
-	}
-	else
-	{
-		tchar = ':';
-		rchar = 0x01;
-	}
+		f_abs = 1234567;
+		unsigned char valuetext[7];
+		ultoa(f_abs, valuetext, 10);
+		Write_LCD (lcd_l2+6,0);
+		LCD_print_str   (valuetext);
 
 
-    if (menu == 0) Write_LCD (th+2,0);
-    if (menu == 0) Write_LCD (tchar,1);
-  	
-	if (t1.run == 1)
-	{
-		Write_LCD (t1m+2,0);
-    	Write_LCD (rchar,1);
-
-		if (++t1.second==60)         //timer1 
-	    { 
-	        t1.second=0; 
-	        if (++t1.minute==60) 
-	        { 
-	            t1.minute=0; 
-	            //if (++t1.hour==24) 
-	            //{ 
-	            //    t1.hour=0;
-	            //}
-				//LCD_out_t1h;
- 
-	        }
-			LCD_out_s1();
-			LCD_out_m1();			
-			if ((t1.minute%alarm_time_1) == 0)
-			{
-				LCD_out_s1();
-				alarm_out(alarm_mode_1,1);
-
-			}
-			 
-	    } 
-		LCD_out_s1();
-		LCD_out_m1();
-	}
-	
-	if (t2.run == 1)
-	{
-		Write_LCD (t2m+2,0);
-    	Write_LCD (rchar,1);
-
-		if (++t2.second==60)         //timer2 
-	    { 
-	        t2.second=0; 
-	        if (++t2.minute==60) 
-	        { 
-	            t2.minute=0; 
-	            //if (++t2.hour==24) 
-	            //{ 
-	            //    t2.hour=0;
-	            //}
-				//Write_LCD (t2h,0);
-				//Write_LCD (t2.hour%10+48,1);
- 
-	        }
-			LCD_out_m2();
-			LCD_out_s2();
-			if ((t2.minute%alarm_time_2) == 0)
-			{
-				LCD_out_s2();
-				alarm_out(alarm_mode_2,2);
-			}
-
- 
-	    } 
-		LCD_out_s2();
-		LCD_out_m2();
+		//read frequency and output
 	}
 
 
 } 
 
-ISR(PCINT0_vect)
+ISR(PCINT0_vect)					//key pressed
 {
 	_delay_ms(80);
 
-	if (((PINB & (1<<k1)) == (1<<k1)) & ((PINB & (1<<k2)) == (1<<k2)) & ((PINB & (1<<kmenu)) == (1<<kmenu)) & ((PINB & (1<<kok)) == (1<<kok))) 
-	//if (!(PINB & (1<<k1)) | !(PINB & (1<<k2)) | !(PINB & (1<<kmenu)) | !(PINB & (1<<kok))) 
+	if (((PINB & (1<<kmode)) == (1<<kmode)) & ((PINB & (1<<kreset)) == (1<<kreset)) & ((PINB & (1<<kmenu)) == (1<<kmenu)) & ((PINB & (1<<kok)) == (1<<kok))) 
+	//if (!(PINB & (1<<kmode)) | !(PINB & (1<<kreset)) | !(PINB & (1<<kmenu)) | !(PINB & (1<<kok))) 
 
 	{
 		timer_key = 0;
@@ -387,8 +285,8 @@ ISR(PCINT0_vect)
 	}
 	_delay_ms(10);
 
-	if (((PINB & (1<<k1)) == (1<<k1)) & ((PINB & (1<<k2)) == (1<<k2)) & ((PINB & (1<<kmenu)) == (1<<kmenu)) & ((PINB & (1<<kok)) == (1<<kok))) 
-	//if (!(PINB & (1<<k1)) | !(PINB & (1<<k2)) | !(PINB & (1<<kmenu)) | !(PINB & (1<<kok))) 
+	if (((PINB & (1<<kmode)) == (1<<kmode)) & ((PINB & (1<<kreset)) == (1<<kreset)) & ((PINB & (1<<kmenu)) == (1<<kmenu)) & ((PINB & (1<<kok)) == (1<<kok))) 
+	//if (!(PINB & (1<<kmode)) | !(PINB & (1<<kreset)) | !(PINB & (1<<kmenu)) | !(PINB & (1<<kok))) 
 	{
 		timer_key = 0;
 		return;
@@ -415,38 +313,34 @@ ISR(PCINT0_vect)
 
 	}
 
-	if ((PINB & (1<<k1)) == 0)         //timer1
+	if ((PINB & (1<<kmode)) == 0)         //mode change
     { 
-		t1.run ^= (0x01);
-		if (!t1.run)
+		switch (mode)
 		{
-			Write_LCD (t1m+2,0);
-	    	Write_LCD (sym_break,1);
+			case 'A':
+				mode = 'R';
+				mode_text(mode);								
+			break;
+			
+			case 'R':
+				mode = 'L';
+				mode_text(mode);								
+			break;
+			
+			case'L':
+				mode = 'A';
+				mode_text(mode);								
+			break;
 		}
-		else
-		{
-			Write_LCD (t1m+2,0);
-    		Write_LCD (rchar,1);
-		}
-
+		
     }
 
 
-	if ((PINB & (1<<k2)) == 0)         //timer2 
+	if ((PINB & (1<<kreset)) == 0)         //reset 
     { 
 		
-	
-		t2.run ^= (0x01);
-		if (!t2.run)
-		{
-			Write_LCD (t2m+2,0);
-	    	Write_LCD (sym_break,1);
-		}
-		else
-		{
-			Write_LCD (t2m+2,0);
-    		Write_LCD (rchar,1);
-		}
+		f_rel=0;
+		l_rel=0;
     }
 
 	if ((PINB & (1<<kok)) == 0)         //Select
@@ -455,46 +349,8 @@ ISR(PCINT0_vect)
 		 switch (menu)
 		 {
 
-			case 1:
-				if (++alarm_mode_1==3) 
-		        { 
-		            alarm_mode_1 = 0;
-		        }
-				Write_LCD (0x8F,0);
-				LCD_out_alarm_sym (alarm_mode_1);
-				break;
-
-			case 2:
-				if (++alarm_time_1==21) 
-		        { 
-		            alarm_time_1=1;
-		        }
-				Write_LCD (0x8A,0);
-				Write_LCD (alarm_time_1/10+48,1);
-				Write_LCD (alarm_time_1%10+48,1);
-				break;
-
-			case 3:
-				if (++alarm_mode_2==3) 
-		        { 
-		            alarm_mode_2 = 0;
-		        }
-				Write_LCD (0x8F,0);
-				LCD_out_alarm_sym (alarm_mode_2);
-				break;
-				break;
-
-			case 4:
-				if (++alarm_time_2==21) 
-		        { 
-		            alarm_time_2=1;
-		        }
-				Write_LCD (0x8A,0);
-				Write_LCD (alarm_time_2/10+48,1);
-				Write_LCD (alarm_time_2%10+48,1);
-				break;
  
-			case 5:
+			/*case 0:
 				if (++t.hour==60) 
 			    { 
 			        t.hour=0; 
@@ -503,7 +359,7 @@ ISR(PCINT0_vect)
 				Write_LCD (t.hour/10+48,1);
 				Write_LCD (t.hour%10+48,1);
 				break;
-			case 6:
+			case 0:
 				if (++t.minute==60) 
 			    { 
 			        t.minute=0; 
@@ -511,8 +367,8 @@ ISR(PCINT0_vect)
 				Write_LCD (0x8E,0);
 				Write_LCD (t.minute/10+48,1);
 				Write_LCD (t.minute%10+48,1);
-				break;
-			case 7:
+				break;*/
+			case 1:
 				if (++contrast==4) 
 			    { 
 			        contrast=1; 
@@ -521,9 +377,9 @@ ISR(PCINT0_vect)
 				Write_LCD (contrast+48,1);
 				comp_contrast(contrast);
 				break;
-			case 8:
-
+			case 2:
 				break;
+
 			
 			default:
 				timer_menu = 0;
@@ -553,33 +409,7 @@ ISR(PCINT0_vect)
 		 timer_menu = 20; 
 		 switch (menu)
 		 {
-			case 1:
-				++menu;
-				Write_LCD (0x80,0);
-  				LCD_print_str   ("Alarm 1:  00 min");
-				Write_LCD (0x8A,0);
-				Write_LCD (alarm_time_1/10+48,1);
-				Write_LCD (alarm_time_1%10+48,1);
-
-				Write_LCD (0x8D,0);
-				break;
-			case 2:
-				++menu;
-				Write_LCD (0x80,0);
-  				LCD_print_str   ("Alarm 2 Modus: ");
-				Write_LCD (0x8F,0);
-				LCD_out_alarm_sym (alarm_mode_2);
-				break;
-			case 3:
-				++menu;
-				Write_LCD (0x80,0);
-  				LCD_print_str   ("Alarm 2:  00 min");
-				Write_LCD (0x8A,0);
-				Write_LCD (alarm_time_2/10+48,1);
-				Write_LCD (alarm_time_2%10+48,1);
-
-				break;
-			case 4:
+/*			case 0:
 				++menu;
 				Write_LCD (0x80,0);
   				LCD_print_str   ("Stunden:   00:00");
@@ -592,7 +422,7 @@ ISR(PCINT0_vect)
 				Write_LCD (t.minute%10+48,1);
 
  				break;
-			case 5:
+			case 0:
 
 				++menu;
 				Write_LCD (0x80,0);
@@ -605,18 +435,8 @@ ISR(PCINT0_vect)
 				Write_LCD (0x8E,0);
 				Write_LCD (t.minute/10+48,1);
 				Write_LCD (t.minute%10+48,1);
-				break;
-			case 6:
-				++menu;
-				Write_LCD (0x80,0);
-  				LCD_print_str   ("Kontrast:      ");
-				Write_LCD (0x8F,0);
-				Write_LCD (contrast+48,1);
-				comp_contrast(contrast);
-
- 				break;
-			case 7:
-
+				break;*/
+			case 1:
 				++menu;
 				Write_LCD (0x80,0);
   				LCD_print_str   ("Batterie:      V");
@@ -628,16 +448,15 @@ ISR(PCINT0_vect)
 				Write_LCD (batt%10+48,1);
 				break;
 
-			case 8:
-
+			case 2:
 				++menu;
 				Write_LCD (0x80,0);
-  				LCD_print_str   ("Enzo/FW Subingen");
+  				LCD_print_str   ("by stoeckli.net ");
 				break;
 			
 			default:
 				menu = 0;
-				LCD_Time();
+				//LCD_Time();
 		 }
 
     }
@@ -645,22 +464,7 @@ ISR(PCINT0_vect)
 	timer_key = 2;
 
 
-	/*
-	temp = 0;
-	_delay_ms(10);
-	while (++temp <= 100)
-	{
-		_delay_ms(10);
-		if ((PINB & 0x1E) == 0x1E)
-		{
-			temp = 202;
-		}
-
-	}
-
-	if (temp < 202)
-	*/
-
+	
 	
 } 
 
@@ -685,7 +489,6 @@ void LCD_Time(void)
 {
 	Write_LCD (0x80,0);
 	LCD_print_str   ("                ");
-	LCD_out_alarm();
 
 	LCD_out_h();
 	LCD_out_m();
@@ -702,7 +505,7 @@ void LCD_Wake(void)
 	PORTB &= 0xFE;
 	_delay_ms(100);
 	LCD_Init();
-	LCD_Time();
+	mode_text(mode);
   	Write_LCD (0xD0,0);
   	LCD_print_str   ("                ");
 
@@ -715,7 +518,6 @@ void LCD_Wake(void)
 	LCD_build(6,pattern6);
 	LCD_build(7,pattern7);
 
-	LCD_out_alarm();
 	batt = battery_voltage();
     battery_symbol();
 	comp_contrast(contrast);
@@ -744,112 +546,10 @@ void LCD_out_s(void)
 	Write_LCD (t.second%10+48,1);
 }
 
-void LCD_out_m1(void)
-{
-	Write_LCD (t1m,0);
-	temp = t1.minute/10 +48;
-	if (temp == 48) temp= temp -16;
-	Write_LCD (temp,1);
-	Write_LCD (t1.minute%10+48,1);
-}
-
-void LCD_out_s1(void)
-{
-	Write_LCD (t1s,0);
-  	Write_LCD (t1.second/10+48,1);
-	Write_LCD (t1.second%10+48,1);
-}
-
-void LCD_out_m2(void)
-{
-	Write_LCD (t2m,0);
-	temp = t2.minute/10 +48;
-	if (temp == 48) temp= temp -16;
-	Write_LCD (temp,1);
-	Write_LCD (t2.minute%10+48,1);
-}
-
-void LCD_out_s2(void)
-{
-	Write_LCD (t2s,0);
-  	Write_LCD (t2.second/10+48,1);
-	Write_LCD (t2.second%10+48,1);
-}
-
-void LCD_out_alarm(void)
-{
-	Write_LCD (t1al,0);
-	LCD_out_alarm_sym (alarm_mode_1);
-
-	Write_LCD (t2al,0);
-	
-	LCD_out_alarm_sym (alarm_mode_2);	
-}
-
-void LCD_out_alarm_sym(unsigned char alarm_mode)
-{
-	switch (alarm_mode)
-	{		
-			case 1:
-			Write_LCD (sym_speaker,1);
-			break;
-		case 2:
-			Write_LCD (sym_light,1);
-			break;
-		default:
-			Write_LCD ('-',1);
-	}
-}
 
 
-void alarm_out(unsigned char alarm_mode, unsigned char alarm_num)
-{
-	char i;
-	int j;
-	switch (alarm_mode)
-	{		
-		case 1:	
 
-					//for(i=0;i<alarm_num;i++)
-					//{
-						/*PORTC |= (1<<p_buzzer);
-						_delay_ms (300);
-						PORTC &=~ (1<<p_buzzer);*/
-						
-						//for (j=0; j< 1200; j++)
-						//{
-							PORTC &=~ (1<<p_buzzer);
-						//	_delay_us (125);
-							//PORTC |= (1<<p_buzzer);
-							//_delay_us (125);	
-						//}
 
-						//_delay_ms (100);
-					//}
-            
-			//break;
-		case 2:
-					PORTD |= (1<<p_light);
-					_delay_ms (200);
-					PORTD &=~ (1<<p_light);
-					_delay_ms (200);
-					if (alarm_num == 2)
-					{					
-						if (alarm_mode == 1) PORTC |= (1<<p_buzzer);
-						PORTD |= (1<<p_light);
-						_delay_ms (200);
-						PORTD &=~ (1<<p_light); 
-						if (alarm_mode == 1) PORTC &=~ (1<<p_buzzer);
-						_delay_ms (200);
-					}
-
-					if (light != 0) PORTD |= (1<<p_light); 
-		break;
-
-			default:
-			Write_LCD ('-',1);
-	}
-}
 
 int battery_voltage(void)
 {
@@ -931,7 +631,7 @@ int comp_contrast(int contrast)
 
 void battery_symbol(void)
 {
-	Write_LCD (0x83,0);
+	Write_LCD (0x8F,0);
 	if (batt > 260) //280 for Cr2032
 	{
 		Write_LCD (sym_full,1);
@@ -945,4 +645,137 @@ void battery_symbol(void)
 	}
 
 		Write_LCD (sym_empty,1);
+}
+
+
+/*****************************************************************************
+*
+*	Function name : SPI_init
+*
+*	Returns :		None
+*
+*	Parameters :	None
+*
+*	Purpose :		Sets up the HW SPI in Master mode, Mode 3
+*					Note -> does notuse SS line to control the DF CS-line.
+*
+******************************************************************************/
+void SPI_init (void)
+{
+	PORTB |= (1<<PB3) | (1<<PB2) | (1<<PB1); // MSt SS not used | (1<<PB0)
+	DDRB |= (1<<DDB2) | (1<<DDB1);		//Set MOSI, SCK  (MSt | (1<<DDB0) SS as outputs)
+
+	//SPSR = (1<<SPI2X);                                      //SPI double speed settings
+	SPCR = (1<<SPE) | (1<<MSTR) | (1<<CPHA) | (1<<CPOL);	//Enable SPI in Master mode, mode 3, Fosc/4
+}
+
+/*****************************************************************************
+*
+*	Function name : SPI_RW
+*
+*	Returns :		Byte read from SPI data register (any value)
+*
+*	Parameters :	Byte to be written to SPI data register (any value)
+*
+*	Purpose :		Read and writes one byte from/to SPI master
+*
+******************************************************************************/
+unsigned char SPI_RW (unsigned char output)
+{
+	unsigned char input;
+	
+	SPDR = output;							//put byte 'output' in SPI data register
+	while(!(SPSR & 0x80));					//wait for transfer complete, poll SPIF-flag
+	input = SPDR;							//read value in SPI data reg.
+	
+	return input;							//return the byte clocked in from SPI slave
+}
+
+
+/*****************************************************************************
+*
+*   Function name : FREQ_display
+*
+*   Returns :       None
+*
+*   Parameters :    None
+*
+*   Purpose :       reads frequency
+*                   and puts it out on the LCD.
+*
+*****************************************************************************/
+void FREQ_display(void)
+{
+	int byteFreq =0;
+	char str1[8] /*={1,2,3,4,5,6,7,8}*/;
+	int z;
+	char outc;
+	long FREQ;
+	
+	SPI_RW ('A');
+	_delay_us(100);
+	
+	FREQ = SPI_RW ('3');
+	//FREQ = 0x00;
+	_delay_us(100);
+	
+
+	byteFreq = SPI_RW ('2');
+	//	byteFreq = 0x3d;
+	FREQ= FREQ * 256 + byteFreq ;
+	_delay_us(100);
+	
+	byteFreq = SPI_RW ('1');
+	//byteFreq = 0x09;
+	FREQ= FREQ * 256 + byteFreq ;
+	_delay_us(100);
+	
+	byteFreq = SPI_RW ('0');
+	//byteFreq = 0x00;
+	FREQ= FREQ * 256 + byteFreq ;
+	_delay_us(100);
+	
+	
+	//FREQ=4685261;
+	//for (z=1; z<7; z++)
+	for (z=5;z >= 0;z--)
+	{
+		outc = FREQ % 10 + 48;
+		//str1[z] = z+48; // Code 48 -> ascii 0++
+		LCD_putc (z,outc);
+		FREQ = FREQ/10;
+	}
+	//
+	////LCD_puts(&str1,0);
+}
+
+void mode_text (char md)
+{
+	switch (md)
+	{
+		case 'A':
+			Write_LCD (lcd_l1,0);
+			LCD_print_str("ABS. FREQUENCY ");
+			Write_LCD (lcd_l2,0);
+			LCD_print_str("              Hz");
+		break;
+			
+		case 'R':
+			Write_LCD (lcd_l1,0);
+			LCD_print_str("REL. FREQUENCY ");
+			Write_LCD (lcd_l2,0);
+			LCD_print_str("              Hz");								
+		break;
+			
+		case'L':
+			Write_LCD (lcd_l1,0);
+			LCD_print_str("LAYER          ");
+			Write_LCD (lcd_l2,0);
+			LCD_print_str("           g/cm ");
+			Write_LCD(lcd_l2+10,0);
+			Write_LCD(sym_micro,1);
+			Write_LCD(lcd_l2+15,0);
+			Write_LCD(sym_squ,1);				
+		break;
+	}
 }
